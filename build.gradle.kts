2e80bd0f6cf0262c9e98cd6dd5d9607bc46684f2
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-04-03 12:09:09 +0200

    Oppdaterte dusseldorf-versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = ext.get("ktorVersion").toString()
-val dusseldorfKtorVersion = "1.1.3.159f4fa"
+val dusseldorfKtorVersion = "1.1.3.2d0b51a"
 
 val wiremockVersion = "2.19.0"
 val openhtmltopdfVersion = "0.0.1-RC19"
@@ -14,7 +14,7 @@ plugins {
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/159f4fa4ebfc36420221eec00934f28175f0a31a/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/2d0b51a6956d1ff797e37527232211027f6970ff/gradle/dusseldorf-ktor.gradle.kts")
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
     }
