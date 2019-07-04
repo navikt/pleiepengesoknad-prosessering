bf7c14afb183f97c8f01d88c371ad6cc7dcaddb9
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-07-04 12:07:58 +0200

    kun preprod

--- src/main/kotlin/no/nav/helse/HttpError.kt
+++ src/main/kotlin/no/nav/helse/HttpError.kt
@@ -1,7 +1,3 @@
 package no.nav.helse
 
-import io.ktor.http.HttpStatusCode
-
-internal class HttpError(private val httpStatus : Int, message: String) : Throwable("HTTP $httpStatus -> $message") {
-    internal fun httpStatusCode() = if (httpStatus < 0) null else HttpStatusCode.fromValue(httpStatus)
-}
+internal class HttpError(httpStatus : Int, message: String) : Throwable("HTTP $httpStatus -> $message")
