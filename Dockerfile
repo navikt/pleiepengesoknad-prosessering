08085f87897f7ca9b13a7556fa1792f8d06b213c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-07-10 10:09:52 +0200

    shadowjar

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM navikt/java:11
 
-COPY build/libs/*.jar ./
+COPY build/libs/app.jar ./
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val ktorVersion = ext.get("ktorVersion").toString()
 val dusseldorfKtorVersion = "1.2.2.8f413ad"
@@ -8,19 +9,17 @@ val openhtmltopdfVersion = "0.0.1-RC21"
 val slf4jVersion = ext.get("slf4jVersion").toString()
 val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
 val kafkaEmbeddedEnvVersion = "2.1.1"
-val kafkaVersion = "2.0.1" // Aliigned med version fra kafka-embedded-env
+val kafkaVersion = "2.0.1" // Alligned med version fra kafka-embedded-env
 
 val mainClass = "no.nav.helse.PleiepengesoknadProsesseringKt"
 
 plugins {
     kotlin("jvm") version "1.3.40"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 buildscript {
     apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/8f413ad909a79e6f5e5897f43f009152ab2f0f35/gradle/dusseldorf-ktor.gradle.kts")
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40")
-    }
 }
 
 dependencies {
@@ -75,24 +74,15 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.named<Jar>("jar") {
-    baseName = "app"
-
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("app")
+    archiveClassifier.set("")
     manifest {
-        attributes["Main-Class"] = mainClass
-        attributes["Class-Path"] = configurations["compile"].map {
-            it.name
-        }.joinToString(separator = " ")
-    }
-
-    configurations["compile"].forEach {
-        val file = File("$buildDir/libs/${it.name}")
-        if (!file.exists())
-            it.copyTo(file)
+        attributes(
+            mapOf(
+                "Main-Class" to mainClass
+            )
+        )
     }
 }
 
