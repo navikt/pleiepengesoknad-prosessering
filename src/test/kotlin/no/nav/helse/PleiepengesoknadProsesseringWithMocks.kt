8b1b0d806c53ae5d3ff9b98fc0e81243f0ed87af
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-08-06 13:09:26 +0200

    Oppdaterte Tester til å bruke Azure mot pleiepenger-* apps

--- src/test/kotlin/no/nav/helse/PleiepengesoknadProsesseringWithMocks.kt
+++ src/test/kotlin/no/nav/helse/PleiepengesoknadProsesseringWithMocks.kt
@@ -1,25 +1,41 @@
 package no.nav.helse
 
+import com.github.tomakehurst.wiremock.WireMockServer
 import io.ktor.server.testing.withApplication
+import no.nav.helse.dusseldorf.ktor.testsupport.asArguments
+import no.nav.helse.dusseldorf.ktor.testsupport.wiremock.WireMockBuilder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-private val logger: Logger = LoggerFactory.getLogger("nav.PleiepengesoknadProsesseringWithMocks")
 
 class PleiepengesoknadProsesseringWithMocks {
     companion object {
+        private val logger: Logger = LoggerFactory.getLogger(PleiepengesoknadProsesseringWithMocks::class.java)
 
         @JvmStatic
         fun main(args: Array<String>) {
 
-            val wireMockServer = WiremockWrapper.bootstrap(port = 8091)
+            val wireMockServer: WireMockServer = WireMockBuilder()
+                .withPort(8091)
+                .withNaisStsSupport()
+                .withAzureSupport()
+                .build()
+                .stubPleiepengerDokumentHealth()
+                .stubPleiepengerJoarkHealth()
+                .stubPleiepengerOppgaveHealth()
+                .stubJournalfor()
+                .stubOpprettOppgave()
+                .stubLagreDokument()
+                .stubSlettDokument()
+                .stubAktoerRegisterGetAktoerId("29099012345", "123456")
+
             val kafkaEnvironment = KafkaWrapper.bootstrap()
 
-            val testArgs = TestConfiguration.asArray(TestConfiguration.asMap(
+            val testArgs = TestConfiguration.asMap(
                 wireMockServer = wireMockServer,
                 kafkaEnvironment = kafkaEnvironment,
                 port = 8092
-            ))
+            ).asArguments()
 
             Runtime.getRuntime().addShutdownHook(object : Thread() {
                 override fun run() {
--- src/test/kotlin/no/nav/helse/TestConfiguration.kt
+++ src/test/kotlin/no/nav/helse/TestConfiguration.kt
@@ -2,6 +2,9 @@ package no.nav.helse
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import no.nav.common.KafkaEnvironment
+import no.nav.helse.dusseldorf.ktor.testsupport.jws.ClientCredentials
+import no.nav.helse.dusseldorf.ktor.testsupport.wiremock.getAzureV2WellKnownUrl
+import no.nav.helse.dusseldorf.ktor.testsupport.wiremock.getNaisStsWellKnownUrl
 
 object TestConfiguration {
 
@@ -9,28 +12,37 @@ object TestConfiguration {
         wireMockServer: WireMockServer? = null,
         kafkaEnvironment: KafkaEnvironment? = null,
         port : Int = 8080,
-        tokenUrl : String? = wireMockServer?.getTokenUrl(),
-        authorizedSystems : String? = wireMockServer?.getSubject(),
         aktoerRegisterBaseUrl : String? = wireMockServer?.getAktoerRegisterBaseUrl(),
         pleiepengerOppgaveBaseUrl : String? = wireMockServer?.getPleiepengerOppgaveBaseUrl(),
         pleiepengerJoarkBaseUrl : String? = wireMockServer?.getPleiepengerJoarkBaseUrl(),
-        pleiepeingerDokumentBaseUrl : String? = wireMockServer?.getPleiepengerDokumentBaseUrl(),
-        clientSecret : String? = "foo"
+        pleiepeingerDokumentBaseUrl : String? = wireMockServer?.getPleiepengerDokumentBaseUrl()
     ) : Map<String, String>{
         val map = mutableMapOf(
             Pair("ktor.deployment.port","$port"),
-            Pair("nav.auth.clients.0.alias", "nais-sts"),
-            Pair("nav.auth.clients.0.client_id", "srvpps-prosessering"),
-            Pair("nav.auth.clients.0.token_endpoint", "$tokenUrl"),
-            Pair("nav.rest_api.authorized_systems","$authorizedSystems"),
             Pair("nav.aktoer_register_base_url","$aktoerRegisterBaseUrl"),
             Pair("nav.pleiepenger_oppgave_base_url","$pleiepengerOppgaveBaseUrl"),
             Pair("nav.pleiepenger_joark_base_url","$pleiepengerJoarkBaseUrl"),
             Pair("nav.pleiepenger_dokument_base_url","$pleiepeingerDokumentBaseUrl")
         )
 
-        clientSecret?.let {
-            map["nav.auth.clients.0.client_secret"] = it
+        // Clients
+        if (wireMockServer != null) {
+            map["nav.auth.clients.0.alias"] = "nais-sts"
+            map["nav.auth.clients.0.client_id"] = "srvpps-prosessering"
+            map["nav.auth.clients.0.client_secret"] = "very-secret"
+            map["nav.auth.clients.0.discovery_endpoint"] = wireMockServer.getNaisStsWellKnownUrl()
+        }
+
+        if (wireMockServer != null) {
+            map["nav.auth.clients.1.alias"] = "azure-v2"
+            map["nav.auth.clients.1.client_id"] = "pleiepengesoknad-prosessering"
+            map["nav.auth.clients.1.private_key_jwk"] = ClientCredentials.ClientA.privateKeyJwk
+            map["nav.auth.clients.1.certificate_hex_thumbprint"] = ClientCredentials.ClientA.certificateHexThumbprint
+            map["nav.auth.clients.1.discovery_endpoint"] = wireMockServer.getAzureV2WellKnownUrl()
+            map["nav.auth.scopes.lagre-dokument"] = "pleiepenger-dokument/.default"
+            map["nav.auth.scopes.slette-dokument"] = "pleiepenger-dokument/.default"
+            map["nav.auth.scopes.journalfore"] = "pleiepenger-joark/.default"
+            map["nav.auth.scopes.opprette-oppgave"] = "pleiepenger-oppgave/.default"
         }
 
         kafkaEnvironment?.let {
@@ -40,13 +52,5 @@ object TestConfiguration {
         }
 
         return map.toMap()
-    }
-
-    fun asArray(map : Map<String, String>) : Array<String>  {
-        val list = mutableListOf<String>()
-        map.forEach { configKey, configValue ->
-            list.add("-P:$configKey=$configValue")
-        }
-        return list.toTypedArray()
     }
 }
