0ede81301e85a35f987835b05680ee5a4d9e5edf
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-08-06 12:34:11 +0200

    AccessTokenClientResolver gir ikke cachet client. Oppdatert AktoerGateway w/scopes

--- src/main/kotlin/no/nav/helse/auth/AccessTokenClientResolver.kt
+++ src/main/kotlin/no/nav/helse/auth/AccessTokenClientResolver.kt
@@ -2,26 +2,30 @@ package no.nav.helse.auth
 
 import no.nav.helse.dusseldorf.ktor.auth.Client
 import no.nav.helse.dusseldorf.ktor.auth.ClientSecretClient
-import no.nav.helse.dusseldorf.ktor.health.HealthCheck
-import no.nav.helse.dusseldorf.ktor.health.Healthy
-import no.nav.helse.dusseldorf.ktor.health.Result
-import no.nav.helse.dusseldorf.ktor.health.UnHealthy
-import no.nav.helse.dusseldorf.oauth2.client.CachedAccessTokenClient
+import no.nav.helse.dusseldorf.ktor.auth.PrivateKeyClient
+import no.nav.helse.dusseldorf.oauth2.client.FromCertificateHexThumbprint
+import no.nav.helse.dusseldorf.oauth2.client.FromJwk
+import no.nav.helse.dusseldorf.oauth2.client.SignedJwtAccessTokenClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 internal class AccessTokenClientResolver(
     clients : Map<String, Client>
-) : HealthCheck {
+) {
 
     companion object {
-        private val logger: Logger = LoggerFactory.getLogger("nav.AccessTokenClientResolver")
+        private val logger: Logger = LoggerFactory.getLogger(AccessTokenClientResolver::class.java)
         private const val NAIS_STS_ALIAS = "nais-sts"
+        private const val AZURE_V2_ALIAS = "azure-v2"
     }
 
     private val naisStsClient = clients.getOrElse(NAIS_STS_ALIAS) {
         throw IllegalStateException("Client[$NAIS_STS_ALIAS] må være satt opp.")
     } as ClientSecretClient
+
+    private val azureV2Client = clients.getOrElse(AZURE_V2_ALIAS) {
+        throw IllegalStateException("Client[$AZURE_V2_ALIAS] må være satt opp.")
+    } as PrivateKeyClient
 
     private val naisStsAccessTokenClient = NaisStsAccessTokenClient(
         clientId = naisStsClient.clientId(),
@@ -29,20 +33,15 @@ internal class AccessTokenClientResolver(
         tokenEndpoint = naisStsClient.tokenEndpoint()
     )
 
-    private val cachedNaisStsAccessTokenClient = CachedAccessTokenClient(naisStsAccessTokenClient)
+    private val azureV2AccessTokenClient = SignedJwtAccessTokenClient(
+        clientId = azureV2Client.clientId(),
+        tokenEndpoint = azureV2Client.tokenEndpoint(),
+        privateKeyProvider = FromJwk(azureV2Client.privateKeyJwk),
+        keyIdProvider = FromCertificateHexThumbprint(azureV2Client.certificateHexThumbprint)
+    )
 
-    internal fun oppgaveAccessTokenClient() = cachedNaisStsAccessTokenClient
-    internal fun dokumentAccessTokenClient() = cachedNaisStsAccessTokenClient
-    internal fun joarkAccessTokenClient() = cachedNaisStsAccessTokenClient
-    internal fun aktoerRegisterAccessTokenClient() = cachedNaisStsAccessTokenClient
-
-    override suspend fun check(): Result {
-        return try {
-            naisStsAccessTokenClient.getAccessToken(setOf("openid"))
-            Healthy("NaisStsAccessTokenClient", "Healthy!")
-        } catch (cause: Throwable) {
-            logger.error("Feil ved henting av access token fra Nais STS", cause)
-            UnHealthy("NaisStsAccessTokenClient", "Unhealthy!")
-        }
-    }
+    internal fun oppgaveAccessTokenClient() = azureV2AccessTokenClient
+    internal fun dokumentAccessTokenClient() = azureV2AccessTokenClient
+    internal fun joarkAccessTokenClient() = azureV2AccessTokenClient
+    internal fun aktoerRegisterAccessTokenClient() = naisStsAccessTokenClient
 }
