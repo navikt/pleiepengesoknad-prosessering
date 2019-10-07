5b07a577cf9d89de11899dc58d99dde12c2e9fbd
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-10-07 13:18:20 +0200

    gradle@5.6.2,ktor@1.2.5,kafka-embedded-env@2.2.3,kotlin@1.3.50

--- src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/Topics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/Topics.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.prosessering.v1.asynkron
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.dusseldorf.ktor.jackson.dusseldorfConfigured
@@ -44,7 +45,9 @@ internal object Topics {
 }
 
 internal abstract class SerDes<V> : Serializer<V>, Deserializer<V> {
-    protected val objectMapper = jacksonObjectMapper().dusseldorfConfigured()
+    protected val objectMapper = jacksonObjectMapper()
+        .dusseldorfConfigured()
+        .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
     override fun serialize(topic: String?, data: V): ByteArray? {
         return data?.let {
             objectMapper.writeValueAsBytes(it)
