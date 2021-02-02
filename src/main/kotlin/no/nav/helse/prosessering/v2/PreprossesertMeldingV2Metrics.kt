820f6bf6b6cfef9ebdf1330dca151e4430346164
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-02-02 15:05:29 +0100

    tmp commit
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/helse/prosessering/v2/PreprossesertMeldingV2Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v2/PreprossesertMeldingV2Metrics.kt
@@ -1,7 +1,11 @@
 package no.nav.helse.prosessering.v2
 
-import io.prometheus.client.Counter
-import io.prometheus.client.Histogram
+import no.nav.helse.prosessering.v1.barnetsAlderHistogram
+import no.nav.helse.prosessering.v1.barnetsAlderIUkerCounter
+import no.nav.helse.prosessering.v1.idTypePaaBarnCounter
+import no.nav.helse.prosessering.v1.jaNeiCounter
+import no.nav.helse.prosessering.v1.periodeSoknadGjelderIUkerHistogram
+import no.nav.helse.prosessering.v1.valgteArbeidsgivereHistogram
 import no.nav.helse.utils.*
 import no.nav.k9.søknad.felles.personopplysninger.Barn
 import no.nav.k9.søknad.ytelse.psb.v1.PleiepengerSyktBarn
@@ -11,48 +15,6 @@ import java.time.temporal.ChronoUnit.WEEKS
 
 private val ZONE_ID = ZoneId.of("Europe/Oslo")
 
-private val periodeSoknadGjelderIUkerHistogram = Histogram.build()
-    .buckets(0.00, 1.00, 4.00, 8.00, 12.00, 16.00, 20.00, 24.00, 28.00, 32.00, 36.00, 40.00, 44.00, 48.00, 52.00)
-    .name("antall_uker_soknaden_gjelder_histogram")
-    .help("Antall uker søknaden gjelder")
-    .register()
-
-private val valgteArbeidsgivereHistogram = Histogram.build()
-    .buckets(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
-    .name("antall_valgte_arbeidsgivere_histogram")
-    .help("Antall arbeidsgivere valgt i søknadene")
-    .register()
-
-
-private val gradHistogram = Histogram.build()
-    .buckets(20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0)
-    .name("grad_histogram")
-    .help("Graden søknaden gjelder")
-    .register()
-
-private val barnetsAlderHistogram = Histogram.build()
-    .buckets(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0)
-    .name("barnets_alder_histogram")
-    .help("Alderen på barnet det søkes for")
-    .register()
-
-private val idTypePaaBarnCounter = Counter.build()
-    .name("id_type_paa_barn_counter")
-    .help("Teller for hva slags ID-Type som er benyttet for å identifisere barnet")
-    .labelNames("id_type")
-    .register()
-
-private val jaNeiCounter = Counter.build()
-    .name("ja_nei_counter")
-    .help("Teller for svar på ja/nei spørsmål i søknaden")
-    .labelNames("spm", "svar")
-    .register()
-
-private val barnetsAlderIUkerCounter = Counter.build()
-    .name("barnets_alder_i_uker")
-    .help("Teller for barn under 1 år, hvor mange uker de er.")
-    .labelNames("uker")
-    .register()
 
 internal fun PreprossesertMeldingV2.reportMetrics() {
     val ytelse = søknad.getYtelse<PleiepengerSyktBarn>()
