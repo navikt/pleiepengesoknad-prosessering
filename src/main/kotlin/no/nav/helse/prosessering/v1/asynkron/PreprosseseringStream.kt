53758b01ef37e300e72e46052e78e5bdb5e48379
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-12-14 10:35:01 +0100

    Ignorerer feilende melding

--- src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/PreprosseseringStream.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/PreprosseseringStream.kt
@@ -36,6 +36,7 @@ internal class PreprosseseringStream(
             builder
                 .stream<String, TopicEntry<MeldingV1>>(fromTopic.name, Consumed.with(fromTopic.keySerde, fromTopic.valueSerde))
                 .filter { _, entry -> 1 == entry.metadata.version }
+                .filter { _, entry -> entry.metadata.correlationId != "generated-58bec3e3-66f6-45a0-bbe2-fb23ce858677" }
                 .mapValues { soknadId, entry  ->
                     process(NAME, soknadId, entry) {
                         logger.info("Preprosesserer søknad.")
