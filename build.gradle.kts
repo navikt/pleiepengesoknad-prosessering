8a711a438144d01a838048255b35450f59b58997
albrektsson <emil.albrektsson@nav.no>
2022-10-17 08:16:47 +0200

    Dependency bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,24 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "3.2.1.1-15e2e63"
-//val ktorVersion = ext.get("ktorVersion").toString()
-val ktorVersion = "2.0.3"
-val k9FormatVersion = "6.1.5"
-val slf4jVersion = ext.get("slf4jVersion").toString()
-val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
-
-val openhtmltopdfVersion = "1.0.10"
-val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
-val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
-val handlebarsVersion = "4.3.0"
+val dusseldorfKtorVersion = "3.2.1.2-ce40a5b"
+val ktorVersion = "2.1.2"
+val k9FormatVersion = "7.0.2"
+val kafkaEmbeddedEnvVersion = "3.2.1"
+val kafkaVersion = "3.2.3"
 val fuelVersion = "2.3.1"
+
+val slf4jVersion = "2.0.3"
+val kotlinxCoroutinesVersion = "1.6.4"
+val openhtmltopdfVersion = "1.0.10"
+val handlebarsVersion = "4.3.0"
 val jsonassertVersion = "1.5.1"
 
 val mainClass = "no.nav.helse.PleiepengesoknadProsesseringKt"
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-}
-
-buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/f630f7019b3dad351323575f3eb09beff58e4b8e/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -56,7 +51,9 @@ dependencies {
 
     // Test
     testImplementation("org.apache.kafka:kafka-clients:$kafkaVersion")
-    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
+    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion") {
+        exclude(group = "org.apache.kafka", module = "kafka-clients")
+    }
     testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
